4f21508ea73c909f69730e3a5e076d7693fbb1f2
RIOTOFF <daxkod3243@gmail.com>
2022-11-30 22:20:48 +0300

    Add files via upload

---
+++ FreeNitro.py
@@ -0,0 +1,14 @@
+# Бесплатное Нитро
+
+from time import sleep
+
+code = input("Введите кодовое слово: ")
+
+if code == "SECRETFREENITRO2023":
+    print("Доступ разрешён!...")
+    sleep(5)
+    print("Перейди по ссылке чтобы получить Дискорд Нитро: https://clck.ru/3vyXS")
+    print("НИКОМУ НЕ ГОВОРИ ПРО СПОСОБ!")
+else:
+    print("Доступ запрещён!")
+    print("Пошёл ты в сраку, Хакер!")
